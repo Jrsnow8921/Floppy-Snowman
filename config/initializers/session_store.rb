d9228722f7793faec45a7c8f8be0e51cfa618822
Programmer/Analyst <jrsnow8921@gmail.com>
2015-08-12 11:33:29 -0400

    Mods to session_store  Changes to be committed: 	modified:   app/views/layouts/application.html.haml 	modified:   config/initializers/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-FlappyBird::Application.config.session_store :cookie_store, key: '_flappy-bird_session'
+FlappyBird::Application.config.session_store :cookie_store, key: '_sloppy-bird_session'
